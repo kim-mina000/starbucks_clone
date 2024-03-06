79bc60c06576e8a9389c6f49cb76ec42c72e28c6
kim_mina <mina14712@gmail.com>
2024-03-06 16:45:58 +0900

    0306

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// 검색 영역(.search)클릭 시 강제 포커스 및 제어
+// 검색 영역 div와 input 찾기
+
+const divSearch = document.querySelector('div.search');
+//const searchInput = document.querySelector('div.search input'); // 문서 전체에서 찾게됨 아래와 같이 최적화
+const searchInput = divSearch.querySelector('input');
+
+// 검색 영역을 클릭하면 input 요소를 포커스 하도록 실행
+divSearch.addEventListener('click',function () {
+  searchInput.focus(); //요소에 포커스 강제 적용
+})
