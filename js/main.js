067bc8bf6a3b54a343f584ffed69f2570a1b1f84
kim_mina <mina14712@gmail.com>
2024-03-11 11:45:46 +0900

    0311

--- js/main.js
+++ js/main.js
@@ -153,13 +153,30 @@ gsap.to('.floating3',1.6,{
 
 
 
-const mainMenuItem = document.querySelectorAll('.item');
-const mainMenuItemContents = document.querySelectorAll('.item__contents');
+// const mainMenuItem = [];
+// document.querySelectorAll('.item').array.forEach(element => {
+//   mainMenuItem.push(element)
+// });;
+// const mainMenuItemContents = document.querySelectorAll('.item__contents');
 
-console.log(mainMenuItem);
-console.log(mainMenuItemContents);
+// console.log(mainMenuItem);
+// console.log(mainMenuItemContents);
 
-let num = 0;
+// let num = 0;
 
 
+
+// scrollMagic 사용
+// 그 외 scrollreveal 둘다 많이 쓰는 라이브러리
+const spyEls = document.querySelectorAll('section.scroll-spy');
+console.log(spyEls);
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic.Scene({ //감시할 장면(Scene) 추가 및 옵션지정
+    triggerElement: spyEl, //보여짐 여부를 감시할 요소를 지정
+    triggerHook: 0.7 //화면의 80%지점에서 보여짐 여부 감시 (0~1 사이 지정)
+  }) 
+  .setClassToggle(spyEl,'show') //요소가 화면에 보이면 show 클래스 추가
+  .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당 (필수!)
+  // 라이브러리에서 지정한 문법으로 깊게 이해x  
+});
 
